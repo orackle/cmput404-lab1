3f3d664dbb6ee88b525f403aa1a1807ee5256490
orackle <42990726+orackle@users.noreply.github.com>
2021-01-15 09:16:50 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import requests
 
 # question 2
-# print(requests.__version__)
+print(requests.__version__ + "\n")
+
+
+# question 5
+print(requests.get('http://www.google.com').status_code)
+
 
 # question 10
 print(requests.get('https://raw.githubusercontent.com/orackle/cmput404-lab1/main/script.py').text)
