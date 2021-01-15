1d21458b8b3d600a95d3fed789c88087169dcb8e
orackle <42990726+orackle@users.noreply.github.com>
2021-01-15 08:14:50 -0700

    Update and rename version_info.py to script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+import requests
+
+# question 2
+# print(requests.__version__)
+
+# question 10
+print(requests.get('https://raw.githubusercontent.com/orackle/CMPUT404-lab1/main/script.py').text)
--- version_info.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def display_ver():
-	print(requests.__version__)
-
-display_ver()
